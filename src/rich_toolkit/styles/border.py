6ad3656cafdd4af97f9c74ae2d5b0c2c6ab9c426
Patrick Arminio <patrick.arminio@gmail.com>
2025-03-24 14:57:34 +0000

    Bordered menu

--- src/rich_toolkit/styles/border.py
+++ src/rich_toolkit/styles/border.py
@@ -1,14 +1,15 @@
-from typing import Any, Union, Optional
+from typing import Any, Optional, Union
 
 from rich import box
-from rich.console import Group, RenderableType
 from rich.color import Color
+from rich.console import Group, RenderableType
 from rich.style import Style
+from rich.text import Text
 
+from rich_toolkit._rich_components import Panel
 from rich_toolkit.element import CursorOffset, Element
 from rich_toolkit.input import Input
 from rich_toolkit.menu import Menu
-from rich_toolkit._rich_components import Panel
 
 from .base import BaseStyle
 
@@ -19,9 +20,9 @@ class BorderedStyle(BaseStyle):
     def empty_line(self) -> RenderableType:
         return ""
 
-    def render_input_or_menu(
+    def render_input(
         self,
-        element: Union[Input, Menu],
+        element: Input,
         is_active: bool = False,
         done: bool = False,
         parent: Optional[Element] = None,
@@ -56,18 +57,6 @@ class BorderedStyle(BaseStyle):
             *validation_message,
         )
 
-    def render_input(
-        self,
-        element: Input,
-        is_active: bool = False,
-        done: bool = False,
-        parent: Optional[Element] = None,
-        **metadata: Any,
-    ) -> RenderableType:
-        return self.render_input_or_menu(
-            element, is_active=is_active, done=done, parent=parent, **metadata
-        )
-
     def render_menu(
         self,
         element: Menu,
@@ -76,95 +65,84 @@ class BorderedStyle(BaseStyle):
         parent: Optional[Element] = None,
         **metadata: Any,
     ) -> RenderableType:
-        return self.render_input_or_menu(
-            element, is_active=is_active, done=done, parent=parent, **metadata
+        menu = Text(justify="left")
+
+        selected_prefix = Text(element.current_selection_char + " ")
+        not_selected_prefix = Text(element.selection_char + " ")
+
+        separator = Text("\t" if element.inline else "\n")
+
+        if done:
+            result_content = Text()
+
+            result_content.append(
+                self.render_input_label(element, is_active=is_active, parent=parent)
+            )
+            result_content.append(" ")
+
+            result_content.append(
+                element.options[element.selected]["name"],
+                style=self.console.get_style("result"),
+            )
+
+            return result_content
+
+        for id_, option in enumerate(element.options):
+            if id_ == element.selected:
+                prefix = selected_prefix
+                style = self.console.get_style("selected")
+            else:
+                prefix = not_selected_prefix
+                style = self.console.get_style("text")
+
+            is_last = id_ == len(element.options) - 1
+
+            menu.append(
+                Text.assemble(
+                    prefix,
+                    option["name"],
+                    separator if not is_last else "",
+                    style=style,
+                )
+            )
+
+        if not element.options:
+            menu = Text("No results found", style=self.console.get_style("text"))
+
+        filter = (
+            [
+                Text.assemble(
+                    (element.filter_prompt, self.console.get_style("text")),
+                    (element.text, self.console.get_style("text")),
+                    "\n",
+                )
+            ]
+            if element.allow_filtering
+            else []
         )
 
-    # def render(
-    #     self,
-    #     element: Union[Element, str],
-    #     is_active: bool = False,
-    #     done: bool = False,
-    #     parent: Optional[Element] = None,
-    #     **metadata: Any,
-    # ) -> RenderableType:
-    #     raise
-    #     from rich_toolkit.input import Input
-    #     from rich_toolkit.menu import Menu
-    #     from rich_toolkit.form import Form
-    #     from rich_toolkit.progress import Progress, ProgressLine
+        content: list[RenderableType] = []
 
-    #     title: Optional[str] = None
-    #     validation_message: tuple[str, ...] = ()
+        content.extend(filter)
+        content.append(menu)
 
-    #     # TOOD: from theme
-    #     border_color = Color.parse("white")
+        validation_message: tuple[str, ...] = ()
 
-    #     if isinstance(parent, Form) and isinstance(renderable, Element):
-    #         return renderable.render(is_active=is_active, done=done, parent=parent)
+        if message := self.render_validation_message(element):
+            validation_message = (message,)
 
-    #     if isinstance(renderable, Input):
+        menu = Group(*content)
 
-    #     if isinstance(renderable, Menu):
-    #         title = renderable.render_label()
-
-    #         renderable._should_show_label = False
-    #         renderable._should_show_validation = False
-
-    #     if isinstance(renderable, Progress):
-    #         title = renderable.title
-
-    #         border_color = self._get_animation_colors(
-    #             steps=10,
-    #             breathe=True,
-    #             animation_status=metadata.get("animation_status", "started"),
-    #         )[self.animation_counter % 10]
-
-    #     if isinstance(renderable, Element):
-    #         rendered = renderable.render(
-    #             is_active=is_active,
-    #             done=done,
-    #             parent=parent,
-    #         )
-
-    #         if renderable._cancelled:
-    #             border_color = self.console.get_style("cancelled").color or Color.parse(
-    #                 "red"
-    #             )
-
-    #             if title:
-    #                 title = f"[title.cancelled]{title}[/]"
-
-    #     else:
-    #         rendered = renderable
-
-    #     if isinstance(renderable, ProgressLine):
-    #         return self.render_progress_log_line(
-    #             rendered,
-    #             index=metadata.get("index", 0),
-    #             max_lines=metadata.get("max_lines", -1),
-    #             total_lines=metadata.get("total_lines", -1),
-    #         )
-
-    #     if metadata.get("title", False):
-    #         content = rendered
-    #     else:
-    #         content = Group(
-    #             Panel(
-    #                 rendered,
-    #                 title=title,
-    #                 title_align="left",
-    #                 highlight=is_active,
-    #                 width=50,
-    #                 box=self.box,
-    #                 border_style=Style(color=border_color),
-    #             ),
-    #             *validation_message,
-    #         )
-
-    #     self.animation_counter += 1
-
-    #     return content
+        return Group(
+            Panel(
+                menu,
+                title=self.render_input_label(element),
+                title_align="left",
+                highlight=is_active,
+                box=self.box,
+            ),
+            *validation_message,
+        )
 
     def get_cursor_offset_for_element(self, element: Element) -> CursorOffset:
         from rich_toolkit.input import Input
